19605f05ae8c0d499af4bb5fbf88295436170ff8
Jacob Lane Armiger <jarmiger@hydra1.eecs.utk.edu>
2023-04-01 20:23:01 -0400

    Add printf's

--- main.c
+++ main.c
@@ -45,14 +45,17 @@ A_output(message) struct msg message;
     B-side. This routine will be called whenever the upper layer at the sending side (A) has a message
     to send. It is the job of your protocol to ensure that the data in such a message is delivered in-order,
     and correctly, to the receiving side upper layer. */
-    printf("\nA_OUTPUT");
+    printf("A_OUTPUT\n");
 
     // struct pkt* my_pkt = (struct pkt*)malloc(sizeof(struct pkt));
     struct pkt my_pkt;
 
+    my_pkt.seqnum = 1;
+    my_pkt.acknum = 2;
+    my_pkt.checksum = 20;
     strncpy(my_pkt.payload, message.data, 19);
     my_pkt.payload[19] = '\0';
-    printf(my_pkt.payload);
+
 
     tolayer3(0, my_pkt);
 }
@@ -68,7 +71,7 @@ A_input(packet) struct pkt packet;
     /* where packet is a structure of type pkt. This routine will be called whenever
     a packet sent from the B-side (i.e., as a result of a tolayer3() being done by a B-side procedure)
     arrives at the A-side. packet is the (possibly corrupted) packet sent from the B-side. */
-    printf("\nA_INPUT");
+    printf("\nA_INPUT\n");
 }
 
 /* called when A's timer goes off */
@@ -77,12 +80,14 @@ A_timerinterrupt()
     /* This routine will be called when A's timer expires (thus generating a timer
     interrupt). You'll probably want to use this routine to control the retransmission of packets.
     See starttimer() and stoptimer() below for how the timer is started and stopped. */
+    printf("\nA_TIMER_INTERRUPT\n");
 }
 
 /* the following routine will be called once (only) before any other */
 /* entity A routines are called. You can use it to do any initialization */
 A_init()
-{
+{   
+    printf("\nA_INiT");
     starttimer(0, 10);
 }
 
@@ -94,20 +99,22 @@ B_input(packet) struct pkt packet;
     /* where packet is a structure of type pkt. This routine will be called whenever
     a packet sent from the A-side (i.e., as a result of a tolayer3() being done by a A-side procedure)
     arrives at the B-side. packet is the (possibly corrupted) packet sent from the A-side */
-    printf("\nB_OUTPUT");
-    printf(packet.payload);
+    printf("B_INPUT\n");
+
+    tolayer5(1, packet.payload);
 }
 
 /* called when B's timer goes off */
 B_timerinterrupt()
 {
+    printf("\nB_TIMER_INTERRUPT\n");
 }
 
 /* the following rouytine will be called once (only) before any other */
 /* entity B routines are called. You can use it to do any initialization */
 B_init()
 {
-
+    printf("\nB_INiT");
 }
 
 /****************************************************************
